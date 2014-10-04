6f1d4a22a341e8e8474501ce33a5f48dd3892f7e
Stefan Marr <git@stefan-marr.de>
2014-10-05 00:59:03 +0200

    Added some basic exceptions used throughout.
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/lib/exceptions.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function IllegalStateException(msg) {
+    this.getMessage = function () { return msg; }
+}
+
+function FileNotFoundException(msg) {
+    this.getMessage = function () { return msg; }
+}
