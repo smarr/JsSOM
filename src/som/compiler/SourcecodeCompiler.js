c9bccbb692c5ca60ed7fa9c4a02bc45ce3eccd9b
Stefan Marr <git@stefan-marr.de>
2014-10-05 00:58:00 +0200

    Added SourcecodeCompiler.js
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/som/compiler/SourcecodeCompiler.js
@@ -0,0 +1,59 @@
+'use strict';
+
+function getFile(path, file) {
+    var current = som.core_lib;
+
+    path.split("/").forEach(function (e) {
+        if (current == undefined) {
+            return null;
+        }
+        current = current[e];
+    });
+
+    if (current == undefined || current[file] == undefined) {
+        return null;
+    } else {
+        return current[file];
+    }
+}
+
+function compile(parser, systemClass) {
+    var cgc = new ClassGenerationContext();
+
+    var result = systemClass;
+    // try {
+        parser.classdef(cgc);
+    //} catch (pe) {
+    //    universe.errorExit(pe.toString());
+    //}
+
+    if (systemClass == null) {
+        result = cgc.assemble();
+    } else {
+        cgc.assembleSystemClass(result);
+    }
+
+    return result;
+}
+
+function compileClassFile(path, file, systemClass) {
+    var source = getFile(path, file + ".som");
+    if (source == null) {
+        throw new FileNotFoundException(path + '/' + file + '.som');
+    }
+
+    var result = compile(new Parser(source), systemClass);
+
+    var cname  = result.getName();
+    var cnameC = cname.getString();
+
+    if (file != cnameC) {
+        throw new IllegalStateException("File name " + file
+            + " does not match class name " + cnameC);
+    }
+    return result;
+}
+
+function compileClassString(stmt, systemClass) {
+    return compile(new Parser(stmt), systemClass);
+}
