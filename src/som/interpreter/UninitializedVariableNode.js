3a8709cc66448c9146f5693f3613b5446c7bb5c4
Stefan Marr <git@stefan-marr.de>
2014-10-06 16:21:00 +0200

    Don't ned uninitialized normal var reads/writes
    Everything is known in the first pass of compilation, specifically the index/lexical address.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/UninitializedVariableNode.js
+++ src/som/interpreter/UninitializedVariableNode.js
@@ -1,17 +1,5 @@
 'use strict';
 
-function UninitializedVariableReadNode(variable, contextLevel, source) {
-    ContextualNode.call(this, contextLevel, source);
-    var _this = this;
-
-    this.execute = function (frame) {
-        assert(false); // TODO: need to tell something about the variable to the new read node...
-        var node = VariableReadNodeFactory.create(contextLevel, variable, source);
-        return _this.replace(node).execute(frame);
-    };
-}
-UninitializedVariableReadNode.prototype = Object.create(ContextualNode.prototype);
-
 function UninitializedSuperReadNode(variable, contextLevel, holderClass,
                                     classSide, source) {
     ContextualNode.call(this, contextLevel, source);
@@ -33,15 +21,3 @@ function UninitializedSuperReadNode(variable, contextLevel, holderClass,
     };
 }
 UninitializedSuperReadNode.prototype = Object.create(ContextualNode.prototype);
-
-function UninitializedVariableWriteNode(variable, contextLevel, exp, source) {
-    ContextualNode.call(this, contextLevel, source);
-    var _this = this;
-
-    this.execute = function(frame) {
-        var node = VariableWriteNodeFactory.create(
-            contextLevel, variable.slot, source, exp);
-        return _this.replace(node).executeGeneric(frame);
-    };
-}
-UninitializedVariableWriteNode.prototype = Object.create(ContextualNode.prototype);
