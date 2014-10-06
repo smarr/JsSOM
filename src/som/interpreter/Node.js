a6e11bf8d0ecaca5f45e75ea19aaa63a6980fca8
Stefan Marr <git@stefan-marr.de>
2014-10-06 16:20:10 +0200

    Added support for .adopt and .replace in Node
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/Node.js
+++ src/som/interpreter/Node.js
@@ -1,6 +1,48 @@
 'use strict';
 
 function Node(source) {
-    this.getSource = function () { return source; }
-    this.replace   = function () { notYetImplemented(); }
+    var _this = this;
+
+    this._parent = null;
+
+    this.getSource = function () { return source; };
+
+    this.adopt = function (nodeOrNodes) {
+        if (nodeOrNodes instanceof Array) {
+            for (var i in nodeOrNodes) {
+                nodeOrNodes[i]._parent = _this;
+            }
+        } else {
+            nodeOrNodes._parent = _this;
+        }
+        return nodeOrNodes;
+    };
+
+    this.replace = function (newNode) {
+        var parent   = _this._parent;
+        var replaced = false;
+
+        for (var prop in parent) {
+            if (prop.indexOf("_child") == 0) {
+                if (prop.indexOf("_children") == 0) { // an array with child nodes
+                    var children = parent[prop];
+                    for (var i in children) {
+                        if (children[i] === _this) {
+                            children[i] = newNode
+                        }
+                    }
+                } else { // just a simple child node
+                    if (parent[prop] === _this) {
+                        parent[prop] = newNode;
+                        replaced = true;
+                    }
+                }
+            }
+        }
+
+        if (!replaced) {
+            debugger; // node was not replaced???
+        }
+        return newNode;
+    }
 }
