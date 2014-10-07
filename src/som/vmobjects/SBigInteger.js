b5d1665059b126fbdc6182c4417c05d732b2282c
Stefan Marr <git@stefan-marr.de>
2014-10-08 00:08:10 +0200

    Add some missing functionality in SBigInteger
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vmobjects/SBigInteger.js
+++ src/som/vmobjects/SBigInteger.js
@@ -10,5 +10,34 @@ function SBigInteger(bigintVal) {
     this.getClass = function () {
         return som.integerClass;
     };
+
+    this.primLessThan = function (right) {
+        var result;
+        if (right instanceof SDouble) {
+            result = bigintVal.toJSNumber() < right;
+        } else if (right instanceof SInteger) {
+            result = bigintVal.lesser(right.getEmbeddedInteger());
+        } else {
+            result = bigintVal.lesser(right.getEmbeddedBigInteger());
+        }
+        return (result) ? som.trueObject : som.falseObject;
+    };
+
+    this.primAsString = function () {
+        return universe.newString(bigintVal.toString());
+    };
+    this.primEquals = function (right) {
+        var result;
+        if (right instanceof SBigInteger) {
+            result = bigintVal.equals(right.getEmbeddedBigInteger());
+        } else if (right instanceof SDouble) {
+            result = bigintVal.toJSNumber() == right.getEmbeddedDouble();
+        } else if (right instanceof SInteger) {
+            result = bigintVal.equals(right.getEmbeddedInteger());
+        } else {
+            result = false;
+        }
+        return (result) ? som.trueObject : som.falseObject;
+    };
 }
 SBigInteger.prototype = Object.create(SAbstractObject.prototype);
