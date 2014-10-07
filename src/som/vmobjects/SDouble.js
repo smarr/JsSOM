7c86c10447ad786baa4460e50b73991f52c30475
Stefan Marr <git@stefan-marr.de>
2014-10-07 20:40:43 +0200

    Added missing SDouble functionality
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vmobjects/SDouble.js
+++ src/som/vmobjects/SDouble.js
@@ -10,5 +10,50 @@ function SDouble(doubleVal) {
     this.getClass = function () {
         return som.doubleClass;
     };
+    
+    function asFloat(obj) {
+        if (obj instanceof SDouble) {
+            return obj.getEmbeddedDouble();
+        } else if (obj instanceof SInteger) {
+            return obj.getEmbeddedInteger();
+        }
+        throw new RuntimeException("Cannot coerce " + obj + " to Double!");
+    }
+
+    this.primMultiply = function (right) {
+        return universe.newDouble(doubleVal * asFloat(right));
+    };
+
+    this.primAdd = function (right) {
+        return universe.newDouble(doubleVal + asFloat(right));
+    };
+
+    this.primAsString = function () {
+        return universe.newString(doubleVal.toString());
+    };
+
+    this.primSubtract = function (right) {
+        return universe.newDouble(doubleVal - asFloat(right))
+    };
+
+    this.primDoubleDiv = function (right) {
+        return universe.newDouble(doubleVal / asFloat(right));
+    };
+
+    this.primIntDiv = function (right) {
+        return universe.newInteger(Math.floor(doubleVal / asFloat(right)));
+    };
+
+    this.primModulo = function (right) {
+        return universe.newDouble(doubleVal % asFloat(right));
+    };
+
+    this.primEquals = function (right) {
+        return (doubleVal == asFloat(right)) ? som.trueObject : som.falseObject;
+    };
+
+    this.primLessThan = function (right) {
+        return (doubleVal < asFloat(right)) ? som.trueObject : som.falseObject;
+    }
 }
 SDouble.prototype = Object.create(SAbstractObject.prototype);
