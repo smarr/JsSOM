390e3e6b12b2e3d07ea75ad43b6a00087ba3ed69
Stefan Marr <git@stefan-marr.de>
2014-10-08 23:41:17 +0200

    Added some missing BigInteger primitives
    - those look like they are likely not correct. But, currently, they are also not tested... so... well. a start.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vmobjects/SBigInteger.js
+++ src/som/vmobjects/SBigInteger.js
@@ -24,6 +24,81 @@ function SBigInteger(bigintVal) {
     this.primAsString = function () {
         return universe.newString(bigintVal.toString());
     };
+
+    this.primAdd = function (right) {
+        if (right instanceof SBigInteger) {
+            return universe.newBiginteger(right.getEmbeddedBigInteger().add(bigintVal));
+        } else if (right instanceof SDouble) {
+            return universe.newDouble(bigintVal.toJSNumber() + right.getEmbeddedDouble());
+        } else {
+            return universe.newBiginteger(bigintVal.add(right.getEmbeddedInteger()))
+        }
+    };
+
+    this.primSubtract = function (right) {
+        var result;
+        if (right instanceof SBigInteger) {
+            result = bigintVal.subtract(right.getEmbeddedBigInteger());
+        } else if (right instanceof SDouble) {
+            return universe.newDouble(bigintVal.toJSNumber() - right.getEmbeddedDouble());
+        } else {
+            result = bigintVal.subtract(right.getEmbeddedInteger())
+        }
+        return universe.newBiginteger(result);
+    };
+
+    this.primMultiply = function (right) {
+        var result;
+        if (right instanceof SBigInteger) {
+            result = bigintVal.multiply(right.getEmbeddedBigInteger());
+        } else if (right instanceof SDouble) {
+            return universe.newDouble(bigintVal.toJSNumber() * right.getEmbeddedDouble());
+        } else {
+            result = bigintVal.multiply(right.getEmbeddedInteger())
+        }
+        return universe.newBiginteger(result);
+    };
+
+    this.primDoubleDiv = function (right) {
+        var result;
+        if (right instanceof SBigInteger) {
+            result = bigintVal.toJSNumber() / right.getEmbeddedBigInteger().toJSNumber();
+        } else if (right instanceof SDouble) {
+            result = bigintVal.toJSNumber() / right.getEmbeddedDouble();
+        } else {
+            result = bigintVal.toJSNumber() / right.getEmbeddedInteger();
+        }
+        return  universe.newDouble(result);
+    };
+
+    this.primIntDiv = function (right) {
+        var result;
+        if (right instanceof SBigInteger) {
+            result = bigintVal.divide(right.getEmbeddedBigInteger());
+        } else if (right instanceof SDouble) {
+            return universe.newDouble(bigintVal.toJSNumber()).primIntDiv(right);
+        } else {
+            result = bigintVal.divide(right.getEmbeddedInteger());
+        }
+        return universe.newBiginteger(result);
+    };
+
+    this.primModulo = function (right) {
+        var result;
+        if (right instanceof SBigInteger) {
+            result = bigintVal.mod(right.getEmbeddedBigInteger());
+        } else if (right instanceof SDouble) {
+            return universe.newDouble(bigintVal.toJSNumber()).primModulo(right);
+        } else {
+            result = bigintVal.mod(right.getEmbeddedInteger());
+        }
+        return universe.newBiginteger(result);
+    };
+
+    this.primAnd = function (right) {
+        notYetImplemented(); // not supported by the library and, not sure what semantics should be
+    };
+
     this.primEquals = function (right) {
         var result;
         if (right instanceof SBigInteger) {
