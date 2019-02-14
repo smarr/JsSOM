59eaef2349a84cfde17716c36df413cf7cf42391
Stefan Marr <git@stefan-marr.de>
2019-02-14 10:13:01 +0000

    Make - a proper operator
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/compiler/Lexer.js
+++ src/som/compiler/Lexer.js
@@ -1,16 +1,16 @@
 /*
 * Copyright (c) 2014 Stefan Marr, mail@stefan-marr.de
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -132,8 +132,7 @@ function Lexer(fileContent) {
                 }
                 state.sym = Sym.Separator;
             } else {
-                state.linePos++;
-                state.set(Sym.Minus, "-");
+                lexOperator();
             }
         } else if (isOperator(currentChar())) {
             lexOperator();
@@ -226,6 +225,8 @@ function Lexer(fileContent) {
             match(Sym.At);
         } else if (currentChar() == '%') {
             match(Sym.Per);
+        } else if (currentChar() == '-') {
+            match(Sym.Minus);
         }
     }
 
@@ -323,7 +324,7 @@ function Lexer(fileContent) {
     function isOperator(c) {
         return c == '~'  || c == '&' || c == '|' || c == '*' || c == '/'
             || c == '\\' || c == '+' || c == '=' || c == '>' || c == '<'
-            || c == ','  || c == '@' || c == '%';
+            || c == ','  || c == '@' || c == '%' || c == '-';
     }
 
     function match(s) {
