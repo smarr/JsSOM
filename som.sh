5f52eefe4827be2da4c0631103f00fc1e8d19985
Stefan Marr <git@stefan-marr.de>
2023-01-11 13:03:39 +0000

    Only update submodule when it’s not there yet
    - this is really getting into the way of developing the core-lib…
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som.sh
+++ som.sh
@@ -3,6 +3,10 @@ pushd `dirname $0` > /dev/null
 SCRIPT_PATH=`pwd`
 popd > /dev/null
 
-git --work-tree=${SCRIPT_PATH} submodule update --init --recursive > /dev/null 2>&1
+if [ ! -f "${SCRIPT_PATH}/core-lib/.git" ]
+then
+  echo Initializing core-lib submodule to have access to the required standard library
+  git --work-tree=${SCRIPT_PATH} submodule update --init --recursive > /dev/null 2>&1
+fi
 
 exec node ${SCRIPT_PATH}/src/node.js "$@"
